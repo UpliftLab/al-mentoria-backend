f76cff4ec572adb1af5b3fa9f6ba0aed31bb4865
sboursen <dev.sboursen@gmail.com>
2022-07-07 16:24:59 +0100

    Add validation to reservation model

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,4 +3,7 @@ class Reservation < ApplicationRecord
   has_one :mentor_topic
   has_one :mentor, through: :mentor_topic
   has_one :topic, through: :mentor_topic
+
+  validates :date, presence: true, length: { maximum: 250 },
+                   comparison: { greater_than_or_equal_to: Time.now.utc.to_date }
 end
