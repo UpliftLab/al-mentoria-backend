d84af58adda28801797762fe84f7efc5d4d93ac1
sboursen <dev.sboursen@gmail.com>
2022-07-07 16:41:26 +0100

    Change the dataType of mentor rating

--- db/migrate/20220707143123_create_mentor_topics.rb
+++ db/migrate/20220707143123_create_mentor_topics.rb
@@ -1,7 +1,7 @@
 class CreateMentorTopics < ActiveRecord::Migration[7.0]
   def change
     create_table :mentor_topics do |t|
-      t.string :rating
+      t.float :rating
       t.references :mentor, foreign_key: true
       t.references :topic, foreign_key: true
 
