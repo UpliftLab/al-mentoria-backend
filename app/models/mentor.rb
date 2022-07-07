bef8ebaf39cea283b4ec249fbeadb7df06c38fa9
sboursen <dev.sboursen@gmail.com>
2022-07-07 16:31:06 +0100

    Add validation to mentor model

--- app/models/mentor.rb
+++ app/models/mentor.rb
@@ -2,4 +2,9 @@ class Mentor < ApplicationRecord
   belongs_to :User
   has_many :mentor_topics, dependent: :destroy
   has_many :topics, through: :mentor_topics
+
+  validates :name, presence: true, length: { maximum: 250 }
+  validates :bio, presence: true, length: { maximum: 250 }
+  validates :photo, presence: true, length: { maximum: 250 }, format: { with: %r{\Ahttps?://.+\z},
+                                                                        message: 'only accepts urls' }
 end
