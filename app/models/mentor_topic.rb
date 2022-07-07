261ea69c4325b172b914e0fbe48b69b49cd7dc38
sboursen <dev.sboursen@gmail.com>
2022-07-07 18:24:44 +0100

    Modify the relationship between reseravetions and mentor_topic to a has_many relationship

--- app/models/mentor_topic.rb
+++ app/models/mentor_topic.rb
@@ -1,6 +1,7 @@
 class MentorTopic < ApplicationRecord
   belongs_to :mentor
   belongs_to :topic
+  has_many :reservations
 
   validates :rating, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }
 end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,8 +1,6 @@
 class Reservation < ApplicationRecord
   belongs_to :user
-  has_one :mentor_topic
-  has_one :mentor, through: :mentor_topic
-  has_one :topic, through: :mentor_topic
+  belongs_to :mentor_topic
 
   validates :date, presence: true, length: { maximum: 250 },
                    comparison: { greater_than_or_equal_to: Time.now.utc.to_date }
