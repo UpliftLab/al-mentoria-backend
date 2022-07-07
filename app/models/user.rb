c868b499d0f63491e517f7ecf97030088015221c
sboursen <dev.sboursen@gmail.com>
2022-07-07 15:59:07 +0100

    Add validation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :mentors, dependent: :destroy
-  has_many :reserverations, dependent: :destroy
+  has_many :reservations, dependent: :destroy
+
+  validates :name, presence: true, length: { maximum: 250 }
+  validates :email, presence: true, length: { maximum: 250 }
 end
