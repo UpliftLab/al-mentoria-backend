fabe10f8f98040e43398ddec024e4a73dd75d505
sboursen <dev.sboursen@gmail.com>
2022-07-07 16:00:55 +0100

    Add validation to topic model

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,4 +1,8 @@
 class Topic < ApplicationRecord
   has_many :mentor_topics, dependent: :destroy
   has_many :mentors, through: :mentor_topics
+
+  validates :label, presence: true, length: { maximum: 250 }
+  validates :icon, presence: true, length: { maximum: 250 }, format: { with: %r{\Ahttps?://.+\z},
+                                                                       message: 'only accepts urls' }
 end
