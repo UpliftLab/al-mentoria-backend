d990580ed6d5c6523423abeec1c46c7e51b72437
sboursen <dev.sboursen@gmail.com>
2022-07-07 18:35:20 +0100

    Update the relationship between the mentor_topics and the relationships

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,6 +1,8 @@
 class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :mentor_topic
+  has_one :mentor, through: :mentor_topic
+  has_one :topic, through: :mentor_topic
 
   validates :date, presence: true, length: { maximum: 250 },
                    comparison: { greater_than_or_equal_to: Time.now.utc.to_date }
