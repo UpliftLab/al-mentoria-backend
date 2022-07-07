d2b8b172e935fb364dd794874c5041cbf3943795
sboursen <dev.sboursen@gmail.com>
2022-07-07 16:37:08 +0100

    Add validation to mentor_topic model

--- app/models/mentor_topic.rb
+++ app/models/mentor_topic.rb
@@ -1,4 +1,6 @@
 class MentorTopic < ApplicationRecord
   belongs_to :mentor
   belongs_to :topic
+
+  validates :rating, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }
 end
