323b6d8267892501b12f2ce803c26b2b6faa0055
Mostafa Ahangarha <ahangarha@riseup.net>
2022-07-08 22:12:57 +0300

    Add routing test for root

---
+++ spec/routing/root_routing_spec.rb
@@ -0,0 +1,9 @@
+require "rails_helper"
+
+RSpec.describe 'Root', type: :routing do
+  describe "routing" do
+    it "routes to mentors#index" do
+      expect(get: "/").to route_to("mentors#index")
+    end
+  end
+end
