9130cf06def6245ef58bffe94010baf7667f58b6
Awais Amjed <awaisamjed171@gmail.com>
2022-07-08 14:28:07 +0500

    Remove unnecessary routing tests for mentor_topics

--- spec/routing/mentor_topics_routing_spec.rb
+++ spec/routing/mentor_topics_routing_spec.rb
@@ -6,20 +6,8 @@ RSpec.describe MentorTopicsController, type: :routing do
       expect(get: '/mentor_topics').to route_to('mentor_topics#index')
     end
 
-    it 'routes to #show' do
-      expect(get: '/mentor_topics/1').to route_to('mentor_topics#show', id: '1')
-    end
-
     it 'routes to #create' do
       expect(post: '/mentor_topics').to route_to('mentor_topics#create')
-    end
-
-    it 'routes to #update via PUT' do
-      expect(put: '/mentor_topics/1').to route_to('mentor_topics#update', id: '1')
-    end
-
-    it 'routes to #update via PATCH' do
-      expect(patch: '/mentor_topics/1').to route_to('mentor_topics#update', id: '1')
     end
 
     it 'routes to #destroy' do
