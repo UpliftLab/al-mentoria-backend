55d731cb9965c2be218a0e27c6597223f3038feb
Mostafa Ahangarha <ahangarha@riseup.net>
2022-07-08 23:42:07 +0300

    fix linter issues

--- spec/requests/mentors_spec.rb
+++ spec/requests/mentors_spec.rb
@@ -12,82 +12,82 @@ require 'rails_helper'
 # of tools you can use to make these specs even more expressive, but we're
 # sticking to rails and rspec-rails APIs to keep things simple and stable.
 
-RSpec.describe "/mentors", type: :request do
+RSpec.describe '/mentors', type: :request do
   # This should return the minimal set of attributes required to create a valid
   # Mentor. As you add validations to Mentor, be sure to
   # adjust the attributes here as well.
-  let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
-  }
+  let(:valid_attributes) do
+    skip('Add a hash of attributes valid for your model')
+  end
 
-  let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
-  }
+  let(:invalid_attributes) do
+    skip('Add a hash of attributes invalid for your model')
+  end
 
   # This should return the minimal set of values that should be in the headers
   # in order to pass any filters (e.g. authentication) defined in
   # MentorsController, or in your router and rack
   # middleware. Be sure to keep this updated too.
-  let(:valid_headers) {
+  let(:valid_headers) do
     {}
-  }
+  end
 
-  describe "GET /index" do
-    it "renders a successful response" do
+  describe 'GET /index' do
+    it 'renders a successful response' do
       Mentor.create! valid_attributes
       get mentors_url, headers: valid_headers, as: :json
       expect(response).to be_successful
     end
   end
 
-  describe "GET /show" do
-    it "renders a successful response" do
+  describe 'GET /show' do
+    it 'renders a successful response' do
       mentor = Mentor.create! valid_attributes
       get mentor_url(mentor), as: :json
       expect(response).to be_successful
     end
   end
 
-  describe "POST /create" do
-    context "with valid parameters" do
-      it "creates a new Mentor" do
-        expect {
+  describe 'POST /create' do
+    context 'with valid parameters' do
+      it 'creates a new Mentor' do
+        expect do
           post mentors_url,
                params: { mentor: valid_attributes }, headers: valid_headers, as: :json
-        }.to change(Mentor, :count).by(1)
+        end.to change(Mentor, :count).by(1)
       end
 
-      it "renders a JSON response with the new mentor" do
+      it 'renders a JSON response with the new mentor' do
         post mentors_url,
              params: { mentor: valid_attributes }, headers: valid_headers, as: :json
         expect(response).to have_http_status(:created)
-        expect(response.content_type).to match(a_string_including("application/json"))
+        expect(response.content_type).to match(a_string_including('application/json'))
       end
     end
 
-    context "with invalid parameters" do
-      it "does not create a new Mentor" do
-        expect {
+    context 'with invalid parameters' do
+      it 'does not create a new Mentor' do
+        expect do
           post mentors_url,
                params: { mentor: invalid_attributes }, as: :json
-        }.to change(Mentor, :count).by(0)
+        end.to change(Mentor, :count).by(0)
       end
 
-      it "renders a JSON response with errors for the new mentor" do
+      it 'renders a JSON response with errors for the new mentor' do
         post mentors_url,
              params: { mentor: invalid_attributes }, headers: valid_headers, as: :json
         expect(response).to have_http_status(:unprocessable_entity)
-        expect(response.content_type).to match(a_string_including("application/json"))
+        expect(response.content_type).to match(a_string_including('application/json'))
       end
     end
   end
 
-  describe "DELETE /destroy" do
-    it "destroys the requested mentor" do
+  describe 'DELETE /destroy' do
+    it 'destroys the requested mentor' do
       mentor = Mentor.create! valid_attributes
-      expect {
+      expect do
         delete mentor_url(mentor), headers: valid_headers, as: :json
-      }.to change(Mentor, :count).by(-1)
+      end.to change(Mentor, :count).by(-1)
     end
   end
 end
--- spec/routing/root_routing_spec.rb
+++ spec/routing/root_routing_spec.rb
@@ -1,9 +1,9 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe 'Root', type: :routing do
-  describe "routing" do
-    it "routes to mentors#index" do
-      expect(get: "/").to route_to("mentors#index")
+  describe 'routing' do
+    it 'routes to mentors#index' do
+      expect(get: '/').to route_to('mentors#index')
     end
   end
 end
