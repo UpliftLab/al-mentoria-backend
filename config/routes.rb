c354dcd470817c4d94fd8fc9e75de8c60b2bcba3
Mostafa Ahangarha <ahangarha@riseup.net>
2022-07-08 21:10:11 +0300

    Limit mentors actions in routes and make root route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :reservations, only: %i[index create destroy]
   resources :mentor_topics
   resources :topics
-  resources :mentors do
+  resources :mentors, only: %i[index show create destroy] do
     resources :mentor_topics, path: 'topics', only: %i[index create destroy]
   end
   devise_for :users,
@@ -10,4 +10,6 @@ Rails.application.routes.draw do
       registrations: :registrations,
       sessions: :sessions
     }
+
+  root 'mentors#index'
 end
