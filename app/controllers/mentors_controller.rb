a4e8864fa0e4eeec08f6ee7e5dbbc8ba55176891
Mostafa Ahangarha <ahangarha@riseup.net>
2022-07-08 20:01:59 +0300

    update files using scafold

--- app/controllers/mentors_controller.rb
+++ app/controllers/mentors_controller.rb
@@ -1,2 +1,51 @@
 class MentorsController < ApplicationController
+  before_action :set_mentor, only: %i[ show update destroy ]
+
+  # GET /mentors
+  def index
+    @mentors = Mentor.all
+
+    render json: @mentors
+  end
+
+  # GET /mentors/1
+  def show
+    render json: @mentor
+  end
+
+  # POST /mentors
+  def create
+    @mentor = Mentor.new(mentor_params)
+
+    if @mentor.save
+      render json: @mentor, status: :created, location: @mentor
+    else
+      render json: @mentor.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /mentors/1
+  def update
+    if @mentor.update(mentor_params)
+      render json: @mentor
+    else
+      render json: @mentor.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /mentors/1
+  def destroy
+    @mentor.destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_mentor
+      @mentor = Mentor.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def mentor_params
+      params.require(:mentor).permit(:user_id, :photo, :name, :bio)
+    end
 end
---
+++ spec/routing/mentors_routing_spec.rb
@@ -0,0 +1,21 @@
+require "rails_helper"
+
+RSpec.describe MentorsController, type: :routing do
+  describe "routing" do
+    it "routes to #index" do
+      expect(get: "/mentors").to route_to("mentors#index")
+    end
+
+    it "routes to #show" do
+      expect(get: "/mentors/1").to route_to("mentors#show", id: "1")
+    end
+
+    it "routes to #create" do
+      expect(post: "/mentors").to route_to("mentors#create")
+    end
+
+    it "routes to #destroy" do
+      expect(delete: "/mentors/1").to route_to("mentors#destroy", id: "1")
+    end
+  end
+end
