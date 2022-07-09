457007f81159cdad961cd760acaf5ef8b1478005
sboursen <dev.sboursen@gmail.com>
2022-07-09 15:28:13 +0100

    Limit the available routes for Topics resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :reservations, only: %i[index create destroy]
   resources :mentor_topics
-  resources :topics
+  resources :topics, only: %i[index create destroy]
   resources :mentors, only: %i[index show create destroy] do
     resources :mentor_topics, path: 'topics', only: %i[index create destroy]
   end
