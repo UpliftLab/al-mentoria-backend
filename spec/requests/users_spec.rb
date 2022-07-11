c19e6d17aa99d551867c8db1a2269a17d17865fd
Ammar Hamlaoui <amarhamlaoui@gmail.com>
2022-07-11 17:22:27 +0100

    Add sign_in path to api documentation.

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,7 +1,49 @@
-require 'rails_helper'
+require "swagger_helper"
 
-RSpec.describe 'Users', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe "users", type: :request do
+  path "/users/sign_in" do
+    post("sign-in") do
+      consumes "application/json"
+      parameter name: :users, in: :body, schema: {
+        type: :object,
+        properties: {
+          email: { type: :string },
+          password: { type: :string },
+        },
+        required: ["email", "password"],
+      }
+      response(200, "successful") do
+        let(:users) { { email: "donald@nomail.com", password: "123123" } }
+        after do |example|
+          example.metadata[:response][:content] = {
+            "application/json" => {
+              example: JSON.parse(response.body, symbolize_names: true),
+            },
+          }
+        end
+        run_test!
+      end
+    end
+  end
+
+  path "/users" do
+    parameter name: "name", in: :path, type: :string, description: "name"
+    parameter name: "email", in: :path, type: :string, description: "email"
+    parameter name: "password", in: :path, type: :string, description: "password"
+    post("sign-up") do
+      response(200, "successful") do
+        let(:name) { "donald" }
+        let(:email) { "donald@nomail.com" }
+        let(:password) { "123123" }
+        after do |example|
+          example.metadata[:response][:content] = {
+            "application/json" => {
+              example: JSON.parse(response.body, symbolize_names: true),
+            },
+          }
+        end
+        run_test!
+      end
+    end
   end
 end
