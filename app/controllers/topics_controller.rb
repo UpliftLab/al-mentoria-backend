6ed2f4febd1a589b4e267223723f38b20d446b61
Mostafa Ahangarha <ahangarha@riseup.net>
2022-07-12 14:04:58 +0300

    fix error rendering when fail to create topic

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -17,7 +17,7 @@ class TopicsController < ApplicationController
     if @topic.save
       render json: { data: @topic }, status: :created
     else
-      render json: { error: create_error('Topic create failed!', @topic.errors) }, status: :unprocessable_entity
+      render json: { error: create_error('Topic create failed!', details: @topic.errors) }, status: :unprocessable_entity
     end
   end
 
