4f88f6a29db6ce023db60e626a849265fb5306fa
Awais Amjed <awaisamjed171@gmail.com>
2022-07-14 21:12:03 +0500

    Add Response with token, name and role

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,11 @@
 class UsersController < ApplicationController
+  def me
+    render json: {
+      data: {
+        token: request.headers['Authorization'].split[1],
+        name: current_user.name,
+        role: current_user.role
+      }
+    }
+  end
 end
