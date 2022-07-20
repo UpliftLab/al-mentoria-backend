78c2d2ddfe4bdf17922e04d515803a94ce8b121b
Awais Amjed <awaisamjed171@gmail.com>
2022-07-20 14:05:57 +0500

    Add unique index on label in topics table

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -3,7 +3,7 @@ class Topic < ApplicationRecord
   has_many :mentors, through: :mentor_topics
   has_many :reservations, through: :mentor_topics, dependent: :destroy
 
-  validates :label, presence: true, length: { maximum: 250 }
+  validates :label, presence: true, length: { maximum: 250 }, uniqueness: true
   validates :icon, presence: true, length: { maximum: 250 }, format: { with: %r{\Ahttps?://.+\z},
                                                                        message: 'only accepts urls' }
 end
